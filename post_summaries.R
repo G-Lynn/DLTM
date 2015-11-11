5e6e1588994828ac0038826d8ec5288ff83bb128
G-Lynn <glynn@stat.duke.edu>
2015-11-11 10:20:28 -0500

    updated file structure

--- post_summaries.R
+++ post_summaries.R
@@ -28,7 +28,7 @@ for(t in 1:t.T) Prob_Beta[[t]] = matrix(nrow = K, ncol = V)
 for(t in 1:t.T) Prob_Beta_CI[[t]] = list() 
 for(k in 1:K){
   Beta[[k]]=list()
-  tmp = read.csv(paste(stem,"Post_Summaries/Init_",init,"/Beta_",k,".csv",sep=""),header=F,stringsAsFactors=F)
+  tmp = read.csv(paste(stem,"MCMC_Samples/Init_",init,"/Beta_",k,".csv",sep=""),header=F,stringsAsFactors=F)
   for(t in 1:t.T){
     Beta[[k]][[t]] = matrix(0,nrow = nSamples,ncol = V)
     for(i in 1:nSamples){
@@ -43,7 +43,7 @@ for(k in 1:K){
   }
 }
 
-save(file = paste(stem,"Reproducibility/Beta_",init,".RData",sep=""),Prob_Beta, Prob_Beta_CI)
+save(file = paste(stem,"MCMC_Summaries/Beta_",init,".RData",sep=""),Prob_Beta, Prob_Beta_CI)
 rm(Beta,tmp,Prob_Beta,index_1,index_2)
 
 Eta = list()
@@ -54,7 +54,7 @@ for(t in 1:t.T){
 }
 
 for(t in 1:t.T){
-  tmp = read.csv(paste(stem,"Post_Summaries/Init_",init,"/Eta_",t,".csv",sep=""),header=F,stringsAsFactors=F)
+  tmp = read.csv(paste(stem,"MCMC_Samples/Init_",init,"/Eta_",t,".csv",sep=""),header=F,stringsAsFactors=F)
 
   Eta[[t]] = list()
   for(d in 1:D[t]){
@@ -64,30 +64,30 @@ for(t in 1:t.T){
   }
 }
 
-save(file = paste(stem,"Reproducibility/Eta_",init,".RData",sep=""),Prob_Eta)
+save(file = paste(stem,"MCMC_Summaries/Eta_",init,".RData",sep=""),Prob_Eta)
 rm(Eta,Prob_Eta,tmp)
 
 #I still need to figure out the indexing on Alpha.  
 Alpha_mean=list()
-Alpha_K = read.csv(paste(stem,"Post_Summaries/Init_",init,"/Alpha_",K,".csv",sep=""), header=F,stringsAsFactors=F)
+Alpha_K = read.csv(paste(stem,"MCMC_Samples/Init_",init,"/Alpha_",K,".csv",sep=""), header=F,stringsAsFactors=F)
 Alpha_K = Alpha_K[(burnIn*p+1):(nSamples*p),]
 
 
 for(k in 1:(K-1)){
-    tmp = read.csv(paste(stem,"Post_Summaries/Init_",init,"/Alpha_",k,".csv",sep=""), header=F,stringsAsFactors=F)
+    tmp = read.csv(paste(stem,"MCMC_Samples/Init_",init,"/Alpha_",k,".csv",sep=""), header=F,stringsAsFactors=F)
     tmp = tmp[(burnIn*p+1):(nSamples*p),]
     
     
     Alpha_mean[[k]] = matrix( unlist( mclapply(1:p, function(i) apply( (tmp[tmp[,1]==i,2:(t.T+1)] - Alpha_K[Alpha_K[,1]==i,2:(t.T+1)] ),2,mean), mc.cores = 8 ) ), byrow=T, nrow = p ) 
 }
-save(file = paste(stem,"Reproducibility/Alpha_",init,".RData",sep=""),Alpha_mean)
+save(file = paste(stem,"MCMC_Summaries/Alpha_",init,".RData",sep=""),Alpha_mean)
 rm(Alpha_mean)
 
 Alpha_t = list()
 for(t in 1:t.T) Alpha_t[[t]] = matrix(nrow = (nSamples-burnIn), ncol = K)
 
 for(k in 1:K){
-  tmp = read.csv(paste(stem,"Post_Summaries/Init_",init,"/Alpha_",k,".csv",sep=""), header=F,stringsAsFactors=F)
+  tmp = read.csv(paste(stem,"MCMC_Samples/Init_",init,"/Alpha_",k,".csv",sep=""), header=F,stringsAsFactors=F)
   tmp = tmp[(burnIn*p+1):(nSamples*p),]
 
   for(i in 1:(nSamples-burnIn)){
@@ -107,4 +107,4 @@ for(t in 1:t.T){
   CI.975[,t] = apply(P.z, 2, quantile, .975)
 }
 
-save(file = paste(stem,"Reproducibility/Prob_Z_",init,".RData",sep=""),P.Z.mean, CI.025, CI.975)
+save(file = paste(stem,"MCMC_Summaries/Prob_Z_",init,".RData",sep=""),P.Z.mean, CI.025, CI.975)
