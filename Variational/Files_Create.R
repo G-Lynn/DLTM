c6f5fc2aa696acb4bcdaffbfd0cd3d1aea80f5f0
G-Lynn <glynn@stat.duke.edu>
2017-06-20 14:06:39 -0400

    create input for Blei DTM

---
+++ Variational/Files_Create.R
@@ -0,0 +1,22 @@
+rm(list=ls())
+load("~/SCIOME/Code/SynDataHarmonic.RData")
+foo.mult = "~/Desktop/dtm_release/dtm/example/SynHarmonic-mult.dat"
+foo.seq = "~/Desktop/dtm_release/dtm/example/SynHarmonic-seq.dat"
+file.remove(foo.mult)
+file.remove(foo.seq)
+cat(file = foo.seq, paste(t.T,"\n",sep="") )
+for(t in 1:t.T){
+  cat(file = foo.seq, paste(D[t],"\n",sep=""),append=T)
+  for(d in 1:D[t]){
+    W_dt = W[[t]][W[[t]][,2]==d, 1 ]
+    index =sort( unique(W_dt) )
+    nUnique = length(index)
+    cat(nUnique, file = foo.mult, append=T)
+    for(n in index){
+      count = sum( W_dt == n )
+      cat(paste(" ",n-1,":",count,sep=""), file = foo.mult, append=T)
+    }
+    cat("\n", file = foo.mult, append=T)
+  }
+  
+}
